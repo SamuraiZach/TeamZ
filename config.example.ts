f4e472ece8b99cb36584f5a2e00e0de25ca5d584
BenSegal855 <bensegal@udel.edu>
2021-01-19 00:09:42 -0500

    tweak: more spelling

--- config.example.ts
+++ config.example.ts
@@ -24,7 +24,7 @@ export const MONGO = '';		// Mongo connection string here
 ***REMOVED***
 	SENDER: '',					// The email address all emails should be sent from
 	REPLY_TO: '',				// The replyto address for all emails
-	REPORT_ADRESSES: [			// A list of all the email address to get the weekly report
+***REMOVED***
 		''
 ***REMOVED***
 ***REMOVED***
