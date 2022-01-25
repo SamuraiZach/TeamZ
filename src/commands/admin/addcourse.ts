a7ec7951d13fdcdca38087b1e1ea7c515f7a517c
Ren Ross <briross@udel.edu>
2022-01-25 15:41:16 -0500

    removed command aliases

--- src/commands/admin/addcourse.ts
+++ src/commands/admin/addcourse.ts
@@ -11,7 +11,6 @@ export default class extends Command {
 	'add all course roles, and add the course to the database. ';
 	usage = '<course ID>';
 	runInDM = false;
-	aliases = ['addc', 'createcourse', 'createc'***REMOVED***
 	tempPermissions: ApplicationCommandPermissionData[] = [ADMIN_PERMS***REMOVED***
 
 	options: ApplicationCommandOptionData[] = [{
