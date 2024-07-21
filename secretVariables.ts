416ee9a90db9e791fb19c69932216c9c941cccee
westa <westa@udel.edu>
2024-07-21 14:51:50 -0700

    Changes to secretVariables. More testing on config issue in onboard source code

--- secretVariables.ts
+++ secretVariables.ts
@@ -2,7 +2,6 @@ export const { DB_CONNECTION } = process.env;
 export const { BOT_NAME } = process.env;
 export const { BOT_TOKEN } = process.env;
 export const { BOT_CLIENT_ID } = process.env;
-export const { MONGO } = process.env;
 export const { DB_USERS } = process.env;
 export const { DB_PVQ } = process.env;
 export const { DB_QTAGS } = process.env;
@@ -34,17 +33,71 @@ export const { CHANNEL_ARCHIVE } = process.env;
 export const { CHANNEL_ROLE_SELECT } = process.env;
 export const { ROLE_DROPDOWNS_COURSE_ROLES } = process.env;
 export const { ROLE_DROPDOWNS_ASSIGN_ROLES } = process.env;
-export const { LEVEL_TIER_ROLES } = process.env;
-export const { FIRST_LEVEL } = process.env;
-export const { ENV_GITHUB_TOKEN } = process.env;
-export const { ENV_GITHUB_PROJECT } = process.env;
-export const { PREFIX } = process.env;
-export const { MAINTAINERS } = process.env;
-export const { SEMESTER_ID } = process.env;
-export const { BLACKLIST } = process.env;
 
 ***REMOVED***
 	TOKEN: BOT_TOKEN,
 	CLIENT_ID: BOT_CLIENT_ID,
 	NAME: BOT_NAME
 ***REMOVED***
+
+export const MONGO = process.env;
+
+***REMOVED***
+	CONNECTION: DB_CONNECTION,
+	USERS: DB_USERS,
+	PVQ: DB_PVQ,
+	QTAGS: DB_QTAGS,
+	ASSIGNABLE: DB_ASSIGNABLE,
+	COURSES: DB_COURSES,
+	REMINDERS: DB_REMINDERS,
+	CLIENT_DATA: DB_CLIENT_DATA,
+	POLLS: DB_POLLS
+***REMOVED***
+
+export const GUILDS = {
+	MAIN: GUILD_MAIN,
+	GATEWAY: GUILD_GATEWAY,
+	GATEWAY_INVITE: GUILD_GATEWAY_INVITE
+***REMOVED***
+
+export const ROLES = {
+	ADMIN: ROLE_ADMIN,
+	STUDENT_ADMIN: ROLE_STUDENT_ADMIN,
+	STAFF: ROLE_STAFF,
+	VERIFIED: ROLE_VERIFIED,
+	MUTED: ROLE_MUTED,
+	LEVEL_ONE: ROLE_LEVEL_ONE
+***REMOVED***
+
+***REMOVED***
+	SENDER: EMAIL_SENDER,
+	REPLY_TO: EMAIL_REPLY_TO,
+	REPORT_ADDRESSES: EMAIL_REPORT_ADDRESSES
+***REMOVED***
+
+export const CHANNELS = {
+	ERROR_LOG: CHANNEL_ERROR_LOG,
+	SERVER_LOG: CHANNEL_SERVER_LOG,
+	MEMBER_LOG: CHANNEL_MEMBER_LOG,
+	MOD_LOG: CHANNEL_MOD_LOG,
+	FEEDBACK: CHANNEL_FEEDBACK,
+	SAGE: CHANNEL_SAGE,
+	ANNOUNCEMENTS: CHANNEL_ANNOUNCEMENTS,
+	ARCHIVE: CHANNEL_ARCHIVE,
+	ROLE_SELECT: CHANNEL_ROLE_SELECT
+***REMOVED***
+
+***REMOVED***
+	COURSE_ROLES: ROLE_DROPDOWNS_COURSE_ROLES,
+	ASSIGN_ROLES: ROLE_DROPDOWNS_ASSIGN_ROLES
+***REMOVED***
+
+export const LEVEL_TIER_ROLES = process.env;
+
+export const FIRST_LEVEL = process.env;
+export const ENV_GITHUB_TOKEN = process.env;
+export const ENV_GITHUB_PROJECT = process.env;
+export const PREFIX = process.env;
+export const MAINTAINERS = process.env;
+export const SEMESTER_ID = process.env;
+export const BLACKLIST = process.env;
