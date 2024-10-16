bf91cebec01e9c83b0208aa3eec144749c152c25
Zachary Witte <68294689+Zachwitte21@users.noreply.github.com>
2024-10-16 09:40:17 -0400

    Setup the work environment

---
+++ src/commands/info/feedbackButtons.ts
@@ -0,0 +1,12 @@
+import { Command } from '@lib/types/Command';
+
+export default class extends Command {
+
+	description = 'Provide feedback buttons for users to use on the feedback channel.';
+
+	async run(): Promise<void> {
+		// Code for the command here
+
+	}
+
+}
