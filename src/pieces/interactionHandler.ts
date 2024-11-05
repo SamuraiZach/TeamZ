db45909872d8272ae39abde917feefb61a0129b9
SamuraiZach <81524537+SamuraiZach@users.noreply.github.com>
2024-11-05 16:49:52 -0500

    got rid of feedbackCount and added documentation for testing in interactionHandlers

--- src/pieces/interactionHandler.ts
+++ src/pieces/interactionHandler.ts
@@ -7,6 +7,9 @@ async function register(bot: Client): Promise<void> {
 	bot.on('interactionCreate', i => {
 		if (i.isMessageComponent()) routeComponentInteraction(bot, i);
 	});
+	// When creating a message this portion will run because it see's that message Reactions have been added and will trigger on the initial add of thumbs up and down
+	// need an if statement to verify the interaction just how the one above says i.isMessageComponent it could be the same but try different options
+	bot.on('messageReactionAdd', i => { console.log('see'); });
 }
 
 async function routeComponentInteraction(bot: Client, i: MessageComponentInteraction) {
@@ -15,6 +18,7 @@ async function routeComponentInteraction(bot: Client, i: MessageComponentInterac
 
 export default register;
 function handleBtnPress(bot: Client, i: ButtonInteraction) {
+	console.log('enter maybe');
 	switch (i.customId.split('_')[0] as SageInteractionType) {
 		case SageInteractionType.POLL:
 			handlePollOptionSelect(bot, i);
