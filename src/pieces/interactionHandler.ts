5d25094740aba529c4aab8e7c50abf53743fac38
SamuraiZach <81524537+SamuraiZach@users.noreply.github.com>
2024-11-05 18:00:25 -0500

    added basic information from reaction

--- src/pieces/interactionHandler.ts
+++ src/pieces/interactionHandler.ts
@@ -1,4 +1,4 @@
-import { ButtonInteraction, Client, MessageComponentInteraction } from 'discord.js';
+import { ButtonInteraction, Client, Message, MessageComponentInteraction, MessageReaction, User } from 'discord.js';
 import { handleRpsOptionSelect } from '../commands/fun/rockpaperscissors';
 import { handlePollOptionSelect } from '../commands/fun/poll';
 import { SageInteractionType } from '@lib/types/InteractionType';
@@ -9,7 +9,7 @@ async function register(bot: Client): Promise<void> {
 	});
 	// When creating a message this portion will run because it see's that message Reactions have been added and will trigger on the initial add of thumbs up and down
 	// need an if statement to verify the interaction just how the one above says i.isMessageComponent it could be the same but try different options
-	bot.on('messageReactionAdd', (reaction, user) => { console.log(reaction, '---', user); });
+	bot.on('messageReactionAdd', (reaction, user) => handleUserReaction(bot, reaction.message.embeds[0].description, reaction.emoji.name, user.id));
 }
 
 async function routeComponentInteraction(bot: Client, i: MessageComponentInteraction) {
@@ -26,5 +26,12 @@ function handleBtnPress(bot: Client, i: ButtonInteraction) {
 		case SageInteractionType.RPS:
 			handleRpsOptionSelect(i);
 			break;
+	}
+}
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function handleUserReaction(bot: Client, description: string, choice: string, user: string) {
+	if (user !== '1285236709780619395') {
+		console.log('enter maybe');
+		console.log(description, '   ', choice, '  ', user);
 	}
 }
